1620474e8b51230432b8e91e447017d7f5a35709
Luildson <luildsonsantos@gmail.com>
2024-04-26 22:03:09 -0300

    Add files via upload

---
+++ marcos barbosa/tv box 7/tv box 4/tv box 3/tv box 2/Tv box/script.js
@@ -0,0 +1,19 @@
+function mostrarForm() {
+    const form = document.querySelector(".formulario");
+    form.style.position = "absolute";
+    form.style.left = "50%";
+    form.style.top = "80%";
+    form.style.transform = "translate(-50%, -50%)";
+  }
+function esconderForm(){
+    form.style.left = "-240";
+    
+    form.style.transform = "translate(0)";
+    header("esconderForm")
+  }   
+
+
+
+
+ 
+ 
